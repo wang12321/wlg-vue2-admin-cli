c68c8c15caf356495312c899234ca0fffa03f7a1
FF <1396933943@qq.com>
2022-10-18 10:36:28 +0800

    fix: 修改网络请求

--- src/services/request.js
+++ src/services/request.js
@@ -1,6 +1,6 @@
 /**
  * @author ff
- * @date 2021/7/28
+ * @date 2022/10/12
  * @Description: 配置get post put delete 可自定义方法扩展
  * @update by:
  */
@@ -12,34 +12,63 @@ const request = {
    * @param url 请求地址
    * @param params 请求参数
    */
-  get(url, params) {
+  get(url, params, headers) {
     const config = {
       method: 'get',
       url: url
     }
+    if (headers) {
+      for (const key in headers) {
+        if (headers[key]) {
+          config.headers[key] = headers[key]
+        }
+      }
+    }
     if (params) config.params = params
     return server(config)
   },
-  post(url, params) {
+  post(url, params, headers) {
     const config = {
       method: 'post',
       url: url
     }
+    if (headers) {
+      for (const key in headers) {
+        if (headers[key]) {
+          config.headers[key] = headers[key]
+        }
+      }
+    }
     if (params) config.data = params
     return server(config)
   },
-  put(url, params) {
+
+  put(url, params, headers) {
     const config = {
       method: 'put',
       url: url
     }
+    if (headers) {
+      for (const key in headers) {
+        if (headers[key]) {
+          config.headers[key] = headers[key]
+        }
+      }
+    }
     if (params) config.params = params
     return server(config)
   },
-  delete(url, params) {
+  delete(url, params, headers) {
     const config = {
       method: 'delete',
       url: url
+    }
+    if (headers) {
+      for (const key in headers) {
+        if (headers[key]) {
+          config.headers[key] = headers[key]
+        }
+      }
     }
     if (params) config.params = params
     return server(config)
